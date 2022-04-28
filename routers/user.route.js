ff6205519af82b2b32df463f831df043d7b1ed91
Phan việt Hoàng <55018866+viethoanguet@users.noreply.github.com>
2022-04-29 00:09:21 +0700

    Update user.route.js

--- routers/user.route.js
+++ routers/user.route.js
@@ -3,4 +3,12 @@ const user = require("../controllers/user.controller");
 
 const Router = express.Router();
 
+
+Router.route("/my-profile").get(user.myprofile);
+Router.route("/").post(user.createUser);
+Router.route("/:userId")
+    .get(user.getUser)
+    .put(user.updateUser)
+    .delete(user.deleteUser);
+
 module.exports = Router;
