c5486cf86724bc90a7ed30b95de939ac63706864
lhtrung11 <lhtrung11@gmail.com>
2022-05-28 03:15:24 +0700

    commit hit hit

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,30 +1,32 @@
-// const Customer = require('../models/Customer');
-// exports.getCustomerProfile = async (req, res, next) => {
-//     try {
-//         const { customerId } = req.bookstore;
-//         const customerprofile = await Customer.findById(customerId);
-//         res.status(200).json({
-//             status: 'success',
-//             data: { customerprofile }
-//         })
-//     } catch (error) {
-//         res.json(error);
-//     }
-// }
+const Customer = require("../models/Customer");
+exports.getCustomerProfile = async (req, res, next) => {
+    try {
+        const { customerId } = req.bookstore;
+        const customerprofile = await Customer.findById(customerId);
+        res.status(200).json({
+            status: "success",
+            data: { customerprofile },
+        });
+    } catch (error) {
+        res.json(error);
+    }
+};
 
-// exports.updateCustomerProfile = async (req, res, next) => {
-//     try {
-
-//         const { customerId } = req.bookstore;
-//         // console.log(bookstoreId)
-//         // const bookstore = await Bookstore.findById(bookstoreId);
-//         const newcustomerProfile = await Customer.findByIdAndUpdate(customerId, { ...req.body }, { new: true })
-//         res.status(200).json({
-//             status: 'success',
-//             data: { newcustomerProfile }
-//         })
-
-//     } catch (error) {
-//         res.json(error);
-//     }
-// }
+exports.updateCustomerProfile = async (req, res, next) => {
+    try {
+        const { customerId } = req.bookstore;
+        // console.log(bookstoreId)
+        // const bookstore = await Bookstore.findById(bookstoreId);
+        const newcustomerProfile = await Customer.findByIdAndUpdate(
+            customerId,
+            { ...req.body },
+            { new: true }
+        );
+        res.status(200).json({
+            status: "success",
+            data: { newcustomerProfile },
+        });
+    } catch (error) {
+        res.json(error);
+    }
+};
